14076573787f70239737956cdbd5a43f6c301501
Sebastian Stanski <seba2005st@gmail.com>
2026-09-14 18:57:18 +0200

    add database test

--- src/main/java/server/RedisServer.java
+++ src/main/java/server/RedisServer.java
@@ -1,6 +1,9 @@
 package server;
 
+import command.CommandRegistry;
 import core.Database;
+import persistence.AofService;
+import pubsub.PubSubBroker;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -11,21 +14,25 @@ import java.util.concurrent.Executors;
 public class RedisServer {
     public static void main(String[] args) {
         int port = 6379;
-        Database database = new Database();
 
-        try (ExecutorService threadPool = Executors.newFixedThreadPool(10);
+        Database database = new Database();
+        PubSubBroker broker = new PubSubBroker();
+        AofService aof = new AofService("appendonly.aof");
+        aof.replay(database);
+
+        CommandRegistry registry = new CommandRegistry(database, broker, aof);
+
+        try (ExecutorService threadPool = Executors.newVirtualThreadPerTaskExecutor();
              ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Redis server listening on port:  " + port);
+            System.out.println("Redis server listening on port: " + port);
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("New client from: " + clientSocket.getInetAddress());
 
-                ClientHandler clientHandler = new ClientHandler(clientSocket, database);
-
+                ClientHandler clientHandler = new ClientHandler(clientSocket, registry);
                 threadPool.execute(clientHandler);
             }
-
         } catch (IOException e) {
             System.err.println("Server error: " + e.getMessage());
         }
--- src/test/java/core/DatabaseTest.java
+++ src/test/java/core/DatabaseTest.java
@@ -1,18 +1,44 @@
 package core;
 
-import org.testng.annotations.Test;
-
-import java.util.ArrayList;
-
-import static org.testng.AssertJUnit.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DatabaseTest {
+
     @Test
-    void shouldSetAndGetKey(){
+    void shouldSetAndGetKey() {
         Database db = new Database();
 
-        db.set("set foo bar");
-        assertEquals("$3\r\nbar \r\n", db.get("foo"));
+        db.set("foo", "bar");
+        assertEquals("bar", db.get("foo"));
+    }
 
+    @Test
+    void shouldReturnNullWhenKeyDoesNotExist() {
+        Database db = new Database();
+
+        assertNull(db.get("non_existing"));
+    }
+
+    @Test
+    void shouldExpireKeyAfterTtl() throws InterruptedException {
+        Database db = new Database();
+
+        db.set("temp", "secret", 50L);
+        assertEquals("secret", db.get("temp"));
+
+        Thread.sleep(60);
+        assertNull(db.get("temp"));
+    }
+
+    @Test
+    void shouldIncrementCounter() {
+        Database db = new Database();
+
+        long val1 = db.incr("counter");
+        long val2 = db.incr("counter");
+
+        assertEquals(1, val1);
+        assertEquals(2, val2);
     }
 }
